cbd8186e63e3765158d7af937c7fa7d42b9a57e8
ajermakovics <ajermakovics@users.noreply.github.com>
2018-12-03 08:38:31 +0000

    migrate to lowercase

--- mongo-db-requests.js
+++ mongo-db-requests.js
@@ -1,18 +1,4 @@
 let date_time = require('date-and-time');
-/* a service that increments download count
- in a new collection
- collection ‘downloads’ i.e. 
- _id = com.github.user/repo
- contents: {“YYYYMMDD”: 123}
- where 123 is download count in that day
- each document in collection downloads: 
-{"_id": "com.github....",
-  "downloads": {
-      "20180311": 123,
-      "20180312": 100,
-      ....
-    }
-}*/
 
 /** @return Promise **/
 function logJSONtoDB(artifact, collection) {
@@ -24,35 +10,62 @@ function logJSONtoDB(artifact, collection) {
     var key = 'downloads.' +[date];
     
     return collection.updateOne(
-        {   
-            _id: id 
-        },
-        { 
-            $inc: { 
-                [key]: + 1 
-            } 
-        },
-        {
-            returnOriginal: false,
-            upsert: true
-        }
+        { _id: id },
+        { $inc: { [key]: + 1 } },
+        { returnOriginal: false,  upsert: true }
     ).catch((error) =>{
         console.log('Error from the increment', error);
     });       
 }
-/*
-Task:
-When: GET /downloads/com.github.user/proj -> group and artifact name
-Return JSON: 
-{
-  "week": <nr Downloads in last 7 days>
-  "month": <Downloads in last 30 days>
+
+function incDoc(collection, id, date, amount) {
+    var key = 'downloads.' + [date];
+
+    return collection.updateOne(
+        { _id: id },
+        { $set: {migrated: true}, $inc: { [key]: + amount } },
+        { returnOriginal: false,  upsert: true }
+    ).catch((error) =>{
+        console.log('Error from the increment', error);
+    });
 }
-*/
+
+function setMigrated(collection, id) {
+    return collection.updateOne(
+        { _id: id },
+        { $set: {migrated: true} },
+        { returnOriginal: false,  upsert: true }
+    ).catch((error) => {
+        console.log('Error from the increment', error);
+    });
+}
 
 const DAYS_IN_MS =  24 * 60 * 60 * 1000;
 const SEVEN_DAYS_IN_MILLISECONDS =  7 * DAYS_IN_MS;
 const THIRTY_DAYS_IN_MILLISECONDS =  30 * DAYS_IN_MS;
+
+/** @param {Collection} collection **/
+async function mergeDuplicates (collection) {
+    console.log("collection: ", collection.dbName)
+    var cnt = 0;
+
+    let cursor = collection.find({"migrated": null}, {sort: {"_id": 1}})
+
+    while(await cursor.hasNext()) {
+        var doc = await cursor.next(); // Promise<doc>
+        var realId = doc._id.toLowerCase()
+        if(realId !== doc._id) {
+            console.log("doc", cnt, doc._id, " != ", realId)
+            for(var date in doc.downloads) {
+                await incDoc(collection, realId, date, doc.downloads[date])
+            }
+            await setMigrated(collection, doc._id)
+            cnt++
+        }
+    }
+
+    console.log("done: ", cnt)
+}
 
 function getWeeklyMonthlyStatistics (artifact, collection) {
     let id = artifact.group + ":" + artifact.name;
@@ -80,21 +93,17 @@ function getWeeklyMonthlyCount(doc) {
         var count = downloads[date] || 0;
 
         var download_date_string = date_time.parse(date, 'YYYYMMDD');
-
-        //difference between now and the date in milliseconds 
         var diff_in_milliseconds = now - download_date_string;
 
-        //checks if the download date is within 7 or 30 days range
-        if (diff_in_milliseconds<= SEVEN_DAYS_IN_MILLISECONDS){
-            last_7_days_download_count += count;
-        }
         if (diff_in_milliseconds <= THIRTY_DAYS_IN_MILLISECONDS){
             last_30_days_download_count += count;
+
+            if (diff_in_milliseconds <= SEVEN_DAYS_IN_MILLISECONDS){
+                last_7_days_download_count += count;
+            }
         }
-        //else {
-        //    break;
-        //}
     }
+
     let count_json = {
         week: last_7_days_download_count,
         month: last_30_days_download_count
@@ -104,3 +113,4 @@ function getWeeklyMonthlyCount(doc) {
 }
 module.exports.logJSSONtoDB = logJSONtoDB;
 module.exports.getWeeklyMonthlyStatistics = getWeeklyMonthlyStatistics;
+module.exports.mergeDuplicates = mergeDuplicates;
